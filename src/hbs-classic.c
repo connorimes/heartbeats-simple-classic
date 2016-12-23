31a518337808db37e15fcc289668c75e5d72d0ab
Connor Imes <connor.k.imes@gmail.com>
2016-12-23 12:38:23 -0800

    Some optimizations, bug fixes in error handling, and cleanup

--- src/hbs-classic.c
+++ src/hbs-classic.c
@@ -11,13 +11,22 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include "heartbeats-simple-classic.h"
 
 static uint64_t get_time_ns() {
   static const uint64_t ONE_BILLION = 1000000000;
   return omp_get_wtime() * ONE_BILLION;
+}
+
+static int hbsc_init_fail(int log_fd, void* window_buffer) {
+  int err_save = errno;
+  if (log_fd > 0) {
+    close(log_fd);
+  }
+  free(window_buffer);
+  errno = err_save;
+  return -1;
 }
 
 /**
@@ -40,7 +49,6 @@ int hbsc_init(hbsc_ctx* hb, uint64_t window_size, const char* log_name) {
     errno = EINVAL;
     return -1;
   }
-  int err_save = 0;
 
   // allocate window buffer
   hb->window_buffer = malloc(window_size * record_size);
@@ -48,13 +56,13 @@ int hbsc_init(hbsc_ctx* hb, uint64_t window_size, const char* log_name) {
     return -1;
   }
 
-  hb->log_fd = 0;
-  if (log_name != NULL) {
+  if (log_name == NULL) {
+    hb->log_fd = 0;
+  } else {
     // open log file
     hb->log_fd = open(log_name, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
     if (hb->log_fd < 0) {
-      free(hb->window_buffer);
-      return -1;
+      return hbsc_init_fail(hb->log_fd, hb->window_buffer);
     }
     // write log file header (and verify that writing works)
 #if defined(HEARTBEAT_MODE_ACC)
@@ -66,42 +74,30 @@ int hbsc_init(hbsc_ctx* hb, uint64_t window_size, const char* log_name) {
 #else
     if (hb_log_header(hb->log_fd)) {
 #endif
-      err_save = errno;
-      close(hb->log_fd);
-      free(hb->window_buffer);
-      errno = err_save;
-      return -1;
+      return hbsc_init_fail(hb->log_fd, hb->window_buffer);
     }
   }
 
   hb->start_time = 0;
 
-  // init energymon
 #if defined(HEARTBEAT_MODE_POW) || defined(HEARTBEAT_MODE_ACC_POW)
-  if (energymon_get_default(&hb->em) || hb->em.finit(&hb->em)) {
-    // safer to force NULL values in case users to continue with it after failure
-    memset(&hb->em, 0, sizeof(energymon));
-    return -1;
+  // init energymon
+  if (energymon_get_default(&hb->em) || hb->em.finit == NULL || hb->em.fread == NULL || hb->em.finit(&hb->em)) {
+    return hbsc_init_fail(hb->log_fd, hb->window_buffer);
   }
   hb->start_energy = 0;
 #endif
 
-  // shouldn't fail unless we did something stupid
+  // per the heartbeats API, these should never fail since we verified parameters
 #if defined(HEARTBEAT_MODE_ACC)
-    if (heartbeat_acc_init(&hb->hb, window_size, hb->window_buffer, hb->log_fd, NULL)) {
+  heartbeat_acc_init(&hb->hb, window_size, hb->window_buffer, hb->log_fd, NULL);
 #elif defined(HEARTBEAT_MODE_POW)
-    if (heartbeat_pow_init(&hb->hb, window_size, hb->window_buffer, hb->log_fd, NULL)) {
+  heartbeat_pow_init(&hb->hb, window_size, hb->window_buffer, hb->log_fd, NULL);
 #elif defined(HEARTBEAT_MODE_ACC_POW)
-    if (heartbeat_acc_pow_init(&hb->hb, window_size, hb->window_buffer, hb->log_fd, NULL)) {
+  heartbeat_acc_pow_init(&hb->hb, window_size, hb->window_buffer, hb->log_fd, NULL);
 #else
-    if (heartbeat_init(&hb->hb, window_size, hb->window_buffer, hb->log_fd, NULL)) {
+  heartbeat_init(&hb->hb, window_size, hb->window_buffer, hb->log_fd, NULL);
 #endif
-    err_save = errno;
-    close(hb->log_fd);
-    free(hb->window_buffer);
-    errno = err_save;
-    return -1;
-  }
 
   return 0;
 }
@@ -118,29 +114,27 @@ void hbsc_acc_pow(hbsc_acc_pow_ctx* hb, uint64_t user_tag, uint64_t work, uint64
 #else
 void hbsc(hbsc_ctx* hb, uint64_t user_tag, uint64_t work) {
 #endif
-  if (hb == NULL || hb->window_buffer == NULL) {
+  if (hb == NULL) {
     errno = EINVAL;
     return;
   }
   // start values are last hb's end values
-  const uint64_t start_time = hb->start_time;
-  const uint64_t end_time = get_time_ns();
-  hb->start_time = end_time;
 #if defined(HEARTBEAT_MODE_POW) || defined(HEARTBEAT_MODE_ACC_POW)
   const uint64_t start_energy = hb->start_energy;
-  const uint64_t end_energy = hb->em.fread(&hb->em);
-  hb->start_energy = end_energy;
+  hb->start_energy = hb->em.fread(&hb->em);
 #endif
+  const uint64_t start_time = hb->start_time;
+  hb->start_time = get_time_ns();
   // the first request doesn't actually issue a heartbeat (not enough data yet)
   if (start_time > 0) {
 #if defined(HEARTBEAT_MODE_ACC)
-    heartbeat_acc(&hb->hb, user_tag, work, start_time, end_time, accuracy);
+    heartbeat_acc(&hb->hb, user_tag, work, start_time, hb->start_time, accuracy);
 #elif defined(HEARTBEAT_MODE_POW)
-    heartbeat_pow(&hb->hb, user_tag, work, start_time, end_time, start_energy, end_energy);
+    heartbeat_pow(&hb->hb, user_tag, work, start_time, hb->start_time, start_energy, hb->start_energy);
 #elif defined(HEARTBEAT_MODE_ACC_POW)
-    heartbeat_acc_pow(&hb->hb, user_tag, work, start_time, end_time, accuracy, start_energy, end_energy);
+    heartbeat_acc_pow(&hb->hb, user_tag, work, start_time, hb->start_time, accuracy, start_energy, hb->start_energy);
 #else
-    heartbeat(&hb->hb, user_tag, work, start_time, end_time);
+    heartbeat(&hb->hb, user_tag, work, start_time, hb->start_time);
 #endif
   }
 }
@@ -162,6 +156,12 @@ int hbsc_finish(hbsc_ctx* hb) {
     return -1;
   }
   int ret = 0;
+  // destroy energymon
+#if defined(HEARTBEAT_MODE_POW) || defined(HEARTBEAT_MODE_ACC_POW)
+  if (hb->em.ffinish != NULL) {
+    ret |= hb->em.ffinish(&hb->em);
+  }
+#endif
   if (hb->log_fd > 0) {
     // flush remaining log entries
 #if defined(HEARTBEAT_MODE_ACC)
@@ -178,12 +178,6 @@ int hbsc_finish(hbsc_ctx* hb) {
   }
   // cleanup memory
   free(hb->window_buffer);
-  hb->window_buffer = NULL;
-  // destroy energymon
-#if defined(HEARTBEAT_MODE_POW) || defined(HEARTBEAT_MODE_ACC_POW)
-  ret |= hb->em.ffinish(&hb->em);
-  memset(&hb->em, 0, sizeof(energymon));
-#endif
   return ret;
 }
 
--- test/hbs-classic-test.c
+++ test/hbs-classic-test.c
@@ -1,13 +1,12 @@
 // force assertions
 #undef NDEBUG
 #include <assert.h>
-#include <inttypes.h>
-#include <stdlib.h>
+#include <stdint.h>
 #include <stdio.h>
 #include "heartbeats-simple-classic.h"
 
 static const uint64_t WINDOW_SIZE = 10;
-static const char* LOGFILE = "/dev/null";
+static const char* LOGFILE = "heartbeat.log";
 static const uint64_t ITERATIONS = 20;
 static const uint64_t WORK = 1;
 static const uint64_t ACCURACY = 1;
@@ -15,6 +14,7 @@ static const uint64_t ACCURACY = 1;
 void test_hbsc() {
   hbsc_ctx hb;
   assert(hbsc_init(&hb, WINDOW_SIZE, LOGFILE) == 0);
+  assert(hbsc_get_hb(&hb));
   uint64_t i;
   for (i = 0; i < ITERATIONS; i++) {
     hbsc(&hb, i, WORK);
@@ -25,6 +25,7 @@ void test_hbsc() {
 void test_hbsc_acc() {
   hbsc_acc_ctx hb;
   assert(hbsc_acc_init(&hb, WINDOW_SIZE, LOGFILE) == 0);
+  assert(hbsc_acc_get_hb(&hb));
   uint64_t i;
   for (i = 0; i < ITERATIONS; i++) {
     hbsc_acc(&hb, i, WORK, ACCURACY);
@@ -35,6 +36,7 @@ void test_hbsc_acc() {
 void test_hbsc_pow() {
   hbsc_pow_ctx hb;
   assert(hbsc_pow_init(&hb, WINDOW_SIZE, LOGFILE) == 0);
+  assert(hbsc_pow_get_hb(&hb));
   uint64_t i;
   for (i = 0; i < ITERATIONS; i++) {
     hbsc_pow(&hb, i, WORK);
@@ -45,6 +47,7 @@ void test_hbsc_pow() {
 void test_hbsc_acc_pow() {
   hbsc_acc_pow_ctx hb;
   assert(hbsc_acc_pow_init(&hb, WINDOW_SIZE, LOGFILE) == 0);
+  assert(hbsc_acc_pow_get_hb(&hb));
   uint64_t i;
   for (i = 0; i < ITERATIONS; i++) {
     hbsc_acc_pow(&hb, i, WORK, ACCURACY);
